c747547a7600ebeffa52146ca7ff2cc9e4daa4d3
m_manish <maanish98mdr@gmail.com>
2024-10-23 15:29:33 +0900

    feat : add the react-router-dom for routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import React from 'react'
+import CreateTrip from './create-trip/index.tsx'
+
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />
+	},
+	{
+		path: "/create-trip",
+		element: <CreateTrip />
+	}
+
+])
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 )
